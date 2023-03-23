39071b6c748d3bffcc6c30103c552a6d98977b33
jkulka <kulkajens82@gmail.com>
2023-03-23 12:49:13 +0100

    Tried to fix rects

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 12:08:11 by jkulka            #+#    #+#             */
-/*   Updated: 2023/03/23 12:09:32 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/03/23 12:47:26 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,8 @@ int	main(void)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	t_point start;
-	t_point end;
 	t_rect rect;
 	
-	start.x = 100;
-	start.y = 100;
-	end.x = 200;
-	end.y = 600;
 	
 	rect.x1 = 100;
 	rect.x2 = 200;
@@ -36,8 +30,8 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	// ft_putline(&img, start.x, start.y,end.x,end.y,0x00FF0000);
-	ft_print_square(&img, rect, create_trgb(1, 145, 210,255));
+	ft_putline(&img, 100, 100, 100, 200,0x00FF0000);
+	// ft_print_square(&img, rect, create_trgb(1, 145, 210,255));
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
--- mlx_utils.c
+++ mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 10:17:33 by jkulka            #+#    #+#             */
-/*   Updated: 2023/03/23 12:02:08 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/03/23 12:48:40 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ void	ft_putline(t_data *img, int x1, int y1,int x2, int y2, int color)
 {
 	int temp_x = x1;
 	int temp_y = y1;
-	while (temp_x != x2)
+	while (temp_x != x2  && temp_y != y1)
 	{
 		my_mlx_pixel_put(img, temp_x, temp_y, color);
 		if(y1 < x2)
@@ -59,7 +59,7 @@ void	ft_putline(t_data *img, int x1, int y1,int x2, int y2, int color)
 
 void ft_print_square(t_data *img, t_rect rect,int color)
 {
-	ft_putline(img, rect.x1, rect.y1, rect.x2, rect.y1,color);
+	ft_putline(img, rect.x1, rect.y1, rect.x2, rect.y1,create_trgb(1,52,255,241));
 	ft_putline(img, rect.x2, rect.y1, rect.x2, rect.y2, color);
 	ft_putline(img, rect.x2, rect.y2, rect.x1, rect.y2, color);
 	ft_putline(img, rect.x1, rect.y2,rect.x1, rect.y1, color);
