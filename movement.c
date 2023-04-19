4b9d5f451514fb5ad110478605d12e2efcc4f7be
jkulka <kulkajens82@gmail.com>
2023-04-19 15:10:44 +0200

    feat(movement.c): add movement.c file with necessary includes and headers

---
+++ movement.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movement.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 10:34:27 by jkulka            #+#    #+#             */
+/*   Updated: 2023/04/19 13:38:10 by jkulka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "Libft/src/ft_printf.h"
+#include "MLX42/include/MLX42/MLX42.h"
+
